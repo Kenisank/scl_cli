5a220a250489b16e1c65b859d055b15335556f9a
Kenisank <kenisank1@gmail.com>
2025-05-16 14:03:59 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var rootCommand = new RootCommand("desc ..") { };
+
+        rootCommand.
+
+
+    }
+
+
+    private static void OnHandleAsync()
+    {
+
+    }
+}
