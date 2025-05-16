e8056c9722176b4c07aae030ba843c7d0da480df
Kenisank <kenisank1@gmail.com>
2025-05-16 14:17:36 +0300

    start

--- obj/Debug/net6.0/scl_cli.AssemblyInfo.cs
+++ obj/Debug/net6.0/scl_cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scl_cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a220a250489b16e1c65b859d055b15335556f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("scl_cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scl_cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
